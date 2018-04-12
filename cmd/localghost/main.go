6a667f982dbcd367fcbb60a0777354aceb72ab9d
Albert Salim <albertlimca@gmail.com>
2018-04-12 22:10:26 +0800

    Validate cli arguments

--- cmd/localghost/main.go
+++ cmd/localghost/main.go
@@ -1,18 +1,28 @@
 package main
 
 import (
+	"errors"
 	"flag"
-	"os"
+	"log"
 
 	"github.com/caalberts/localghost/http"
 	"github.com/caalberts/localghost/schema"
 )
 
-var port = flag.String("p", "8080", "port number")
-
 func main() {
+	port := flag.String("p", "8080", "port number")
 	flag.Parse()
-	definition := os.Args[1]
+	args := flag.Args()
+	definition := parseInput(args)
 	schema, _ := schema.FromString(definition)
-	http.NewServer(*port, schema).ListenAndServe()
+	server := http.NewServer(*port, schema)
+	log.Fatal(server.ListenAndServe())
+}
+
+func parseInput(args []string) string {
+	if len(args) < 1 {
+		log.Fatal(errors.New("Please define an endpoint in the format '<METHOD> <PATH> <STATUS_CODE>'. e.g 'GET / 200'"))
+	}
+
+	return args[0]
 }
